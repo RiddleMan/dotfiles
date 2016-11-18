db1ce4a3061c296ba46d47a4d9216b85c7597f40
Artur Ptaszek <artur.ptaszek@gmail.com>
2016-11-18 13:06:37 +0100

    Add git dotfiles to repository
    Also add installation in install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#~/bin/sh
+
+# GIT links
+ln -sf ~/Projects/dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/Projects/dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -sf ~/Projects/dotfiles/git/.gitk ~/.gitk
+
