a656f5802f54d1d50f1e0680711fee502f6c1a64
Artur Ptaszek <artur.ptaszek@gmail.com>
2018-08-30 09:34:36 +0200

    Minor improvements

--- defaults.sh
+++ defaults.sh
@@ -4,3 +4,6 @@ defaults write com.apple.dashboard mcx-disabled -boolean YES && killall Dock
 
 # Turns off Character Accent Menu
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Turns off desktop icons
+defaults write com.apple.finder CreateDesktop false; killall Finder
