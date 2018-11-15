f7d1f693051aa9aff48f975999567d7a53fa172d
Artur Ptaszek <artur.ptaszek@gmail.com>
2018-11-15 16:06:31 +0100

    Update

--- defaults.sh
+++ defaults.sh
@@ -7,3 +7,7 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Turns off desktop icons
 defaults write com.apple.finder CreateDesktop false; killall Finder
+
+# Iterm2
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES_DIR/iterm2"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
