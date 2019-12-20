c3155ebf73c658514047f9fb36896d60629fec89
Artur Ptaszek <artur.ptaszek@gmail.com>
2019-12-20 10:00:14 +0100

    Fixes to the bb script

--- scripts/bb_go_to_repo.sh
+++ scripts/bb_go_to_repo.sh
@@ -9,8 +9,7 @@ historyPath="/tmp/$scriptName.history"
 
 get_repositories_info() {
 	curl -s -u "$BITBUCKET_USER:$BITBUCKET_PASSWORD" \
-		"https://api.bitbucket.org/2.0/user/permissions/repositories?page=$1" \
-		| tr '\r\n' '  '
+		"https://api.bitbucket.org/2.0/user/permissions/repositories?page=$1"
 }
 next_page() {
 	echo "$1" | jq '.next'
@@ -20,25 +19,26 @@ results=""
 currTime=$(date +%s)
 lastSync=$(cat "$lastSyncPath" 2>/dev/null || true)
 
-if [ -n "$lastSync" ] && [ $((currTime - lastSync)) -lt 2592000 ]; then
+if [ -n "$lastSync" ] && [ $((currTime - lastSync)) -lt 2592000 ] && [ -z "$1" ]; then
 	echo "Loading from cache"
 	results=$(cat "$resultsPath")
 else
 	echo "Refreshing a results..."
 	currentResponse=""
-	page=1
+    page=1
 
-	while true; do
-		echo "Downloading page $page..."
-		currentResponse=$(get_repositories_info $page)
-		if [ "$(next_page "$currentResponse")" = "null" ]; then
-			break
-		fi
+    while true; do
+        echo "Downloading page $page..."
+        currentResponse=$(get_repositories_info $page)
+        links=$(echo "$currentResponse" | jq '.values[].repository.links.html.href' | tr -d '\"')
+        results="$results\n$links"
 
-		links=$(echo "$currentResponse" | jq '.values[].repository.links.html.href' | tr -d '\"')
-		results="$results\n$links"
-		page=$((page + 1))
-	done
+        if [ "$(next_page "$currentResponse")" = "null" ]; then
+            break
+        fi
+
+        page=$((page + 1))
+    done
 	echo "$results" > "$resultsPath"
 	echo "$currTime" > "$lastSyncPath"
 	echo "Finished downloading"
