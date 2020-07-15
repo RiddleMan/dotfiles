d29840f792bdb77f99fc7257819afe4731ef2e76
Artur Ptaszek <artur.ptaszek@gmail.com>
2020-07-15 10:08:07 +0200

    Update getSpotifyUrl

--- automation/spotify/getCurrentTrack.js
+++ automation/spotify/getCurrentTrack.js
@@ -2,5 +2,6 @@
 
 function run() {
   var app = Application('Spotify');
-  console.log(app.currentTrack().spotifyUrl())
+  var url = app.currentTrack().spotifyUrl();
+  console.log('https://open.spotify.com/track/' + url.replace('spotify:track:', ''))
 }
