37ae571ac111fc1ccc4e3ee0ee8cd2a881cc2527
Artur Ptaszek <artur.ptaszek@gmail.com>
2021-11-15 22:42:05 +0100

    feat: Shellcheck compliance

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ ln -sf $DOTFILES_DIR/git/.gitk ~/.gitk
 # ZSH links
 ln -sf $DOTFILES_DIR/zsh/.zshrc ~/.zshrc
 
-pushd ${ZSH_CUSTOM:-$ZSH/custom}/plugins
+pushd "${ZSH_CUSTOM:-$ZSH/custom}/plugins"
 git clone https://github.com/iam4x/zsh-iterm-touchbar.git || true
 git clone https://github.com/zsh-users/zsh-autosuggestions || true
 popd
@@ -55,7 +55,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || true
 nvm install --lts --default
 
 # FZF bindings
-$(brew --prefix)/opt/fzf/install \
+"$(brew --prefix)/opt/fzf/install" \
   --no-bash \
   --key-bindings \
   --completion \
