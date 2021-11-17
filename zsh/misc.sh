7cad0c588f6444f00428fc884ac36a3cc69f3c85
Artur Ptaszek <artur.ptaszek@gmail.com>
2021-11-18 00:47:22 +0100

    feat: Add annotation about the performance of NVM
    Closes #28

--- zsh/misc.sh
+++ zsh/misc.sh
@@ -7,7 +7,9 @@ SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 export SSH_AUTH_SOCK
 gpgconf --launch gpg-agent
 
-# NVM
+# NOTE: NVM is the longest job during shell startup
+# Either contribute to NVM or find better alternative
+# like asdf. https://github.com/RiddleMan/dotfiles/issues/31
 source "$DOTFILES_DIR/zsh/nvm.sh"
 
 # Fuzzy search support for ZSH
