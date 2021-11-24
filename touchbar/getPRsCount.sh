b7e29268c5d1a954866a3dab226c103e38384686
Artur Ptaszek <artur.ptaszek@gmail.com>
2021-11-24 22:26:10 +0100

    chore: Remove unused code
    Closes: #20

--- touchbar/getPRsCount.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-repos_list="$(curl -X GET \
-  -u "$BITBUCKET_USER:$BITBUCKET_PASSWORD" \
-  'https://api.bitbucket.org/2.0/repositories?role=member&fields=values.full_name&pagelen=100' | jq '.values[].full_name')"
-
-echo "$repos_list"
