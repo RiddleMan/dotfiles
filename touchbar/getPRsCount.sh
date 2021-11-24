2fdf2eb2cd575ee25ee1440eb152fb8a2a1399c9
Artur Ptaszek <artur.ptaszek@gmail.com>
2021-11-24 22:19:41 +0100

    chore: Cases renames
    Closes: #20

--- touchbar/getPRsCount.sh
+++ touchbar/getPRsCount.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-reposList="$(curl -X GET \
+repos_list="$(curl -X GET \
   -u "$BITBUCKET_USER:$BITBUCKET_PASSWORD" \
   'https://api.bitbucket.org/2.0/repositories?role=member&fields=values.full_name&pagelen=100' | jq '.values[].full_name')"
 
-echo "$reposList"
+echo "$repos_list"
