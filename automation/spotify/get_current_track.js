6d28d44a4d5a85b1b0a5628f5585d9e56672c477
Artur Ptaszek <artur.ptaszek@gmail.com>
2022-11-17 21:49:09 +0100

    feat: Removal of old structure

--- automation/spotify/get_current_track.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env osascript -l JavaScript
-
-function run() {
-  var app = Application('Spotify');
-  var url = app.currentTrack().spotifyUrl();
-  console.log('https://open.spotify.com/track/' + url.replace('spotify:track:', ''))
-}
