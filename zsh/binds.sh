9d9d9b9b87ee0b1fa4830ba2f141e4268557d750
Artur Ptaszek <artur.ptaszek@gmail.com>
2022-11-17 21:54:55 +0100

    feat: Cleanup

--- zsh/binds.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-bindkey '^ ' autosuggest-accept
-bindkey -v '^ ' autosuggest-accept
-
-# Add Ctrl+N, Ctrl+P binding in viins (Insert mode)
-bindkey -v '^P' up-history
-bindkey -v '^N' down-history
