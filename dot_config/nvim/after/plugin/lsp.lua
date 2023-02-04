e07cb9635d0b49e28ed5b087dcff8cb911091ac3
Artur Ptaszek <artur@ptaszek.dev>
2023-02-04 12:02:49 +0100

    feat: Add lsp-zero

---
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,20 @@
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+lsp.nvim_workspace()
+lsp.ensure_installed({
+  'html',
+  'cssls',
+  'tsserver',
+  'ansiblels',
+  'bashls',
+  'dockerls',
+  'eslint',
+  'graphql',
+  'jsonls',
+  'marksman',
+  'rust_analyzer',
+  'vimls'
+})
+
+lsp.setup()
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -2,4 +2,25 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
 end)
