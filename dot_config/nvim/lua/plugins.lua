bd906836f17d3ef86c0ba8c92e9c4e49fd62b4f5
Artur Ptaszek <artur@ptaszek.dev>
2023-02-05 21:52:51 +0100

    feat: Add rose-pine

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -40,4 +40,13 @@ return require('packer').startup(function(use)
       {'nvim-tree/nvim-web-devicons'},
     }
   }
+
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      require("rose-pine").setup()
+      vim.cmd('colorscheme rose-pine')
+    end
+  })
 end)
