f96ce7c18f1711863754e7b9b17f52a8669f7e7a
Artur Ptaszek <artur.ptaszek@gmail.com>
2023-02-06 11:29:16 +0100

    feat: Add lua,latex language servers

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -11,11 +11,32 @@ lsp.ensure_installed({
   'dockerls',
   'eslint',
   'graphql',
+  'ltex',
   'jsonls',
   'marksman',
   'rust_analyzer',
+  'sumneko_lua',
   'yamlls',
   'vimls'
 })
+
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 lsp.setup()
