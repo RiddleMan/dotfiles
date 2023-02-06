f2b7b6597c53a8f7a56628017fb2549f030bd473
Artur Ptaszek <artur.ptaszek@gmail.com>
2023-02-06 13:51:59 +0100

    feat: Add hidden files in telescope

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,24 @@
-require('telescope').load_extension('fzf')
+local telescope = require"telescope"
+
+telescope.setup {
+  pickers = {
+    live_grep = {
+      additional_args = function()
+        return { "--hidden" }
+      end
+    },
+    git_files = {
+      hidden = true
+    },
+    find_files = {
+      hidden = true
+    }
+  }
+}
+telescope.load_extension('fzf')
 
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
