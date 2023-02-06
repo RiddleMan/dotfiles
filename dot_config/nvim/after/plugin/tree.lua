85bb541205bb2c13f4b420b262cfbac67b77e89a
Artur Ptaszek <artur.ptaszek@gmail.com>
2023-02-06 12:06:51 +0100

    feat: Add open on run for tree

--- dot_config/nvim/after/plugin/tree.lua
+++ dot_config/nvim/after/plugin/tree.lua
@@ -1,1 +1,9 @@
-require("nvim-tree").setup()
+require"nvim-tree".setup()
+
+local api = require("nvim-tree.api")
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function()
+    api.tree.open()
+  end
+})
+
