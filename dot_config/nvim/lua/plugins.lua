3641d3aa6cb48e8da07c25c57ec81985a1bf182a
Artur Ptaszek <artur@ptaszek.dev>
2023-02-07 21:19:41 +0100

    feat: Add lualine

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,66 +1,79 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+return require('packer')
+    .startup(function(use)
+      use 'wbthomason/packer.nvim'
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      'neovim/nvim-lspconfig',
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
+      use {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v1.x',
+          requires = {
+              'neovim/nvim-lspconfig',
+              'williamboman/mason.nvim',
+              'williamboman/mason-lspconfig.nvim',
 
-      'hrsh7th/nvim-cmp',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lua',
+              'hrsh7th/nvim-cmp',
+              'hrsh7th/cmp-nvim-lsp',
+              'hrsh7th/cmp-buffer',
+              'hrsh7th/cmp-path',
+              'saadparwaiz1/cmp_luasnip',
+              'hrsh7th/cmp-nvim-lua',
 
-      'L3MON4D3/LuaSnip',
-      'rafamadriz/friendly-snippets',
-    }
-  }
+              'L3MON4D3/LuaSnip',
+              'rafamadriz/friendly-snippets',
+          }
+      }
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+      use {
+          'nvim-treesitter/nvim-treesitter',
+          run = ':TSUpdate'
+      }
 
-  use 'christoomey/vim-tmux-navigator'
+      use 'christoomey/vim-tmux-navigator'
 
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+      use {
+          'nvim-telescope/telescope-fzf-native.nvim',
+          run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+      }
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    requires = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-    }
-  }
+      use {
+          'nvim-telescope/telescope.nvim',
+          branch = '0.1.x',
+          requires = {
+              'nvim-lua/plenary.nvim',
+              'nvim-tree/nvim-web-devicons',
+          }
+      }
 
-  use {
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-      require("rose-pine").setup()
-      vim.cmd('colorscheme rose-pine')
-    end
-  }
+      use {
+          'rose-pine/neovim',
+          as = 'rose-pine',
+          config = function()
+            require("rose-pine").setup()
+            vim.cmd('colorscheme rose-pine')
+          end
+      }
 
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    tag = 'nightly'
-  }
+      use {
+          'nvim-tree/nvim-tree.lua',
+          requires = {
+              'nvim-tree/nvim-web-devicons',
+          },
+          tag = 'nightly'
+      }
 
-  use 'tpope/vim-fugitive'
+      use 'tpope/vim-fugitive'
 
-  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+      use {
+          'akinsho/bufferline.nvim',
+          tag = "v3.*",
+          requires = 'nvim-tree/nvim-web-devicons'
+      }
 
-  use {'jose-elias-alvarez/null-ls.nvim'}
-end)
+      use { 'jose-elias-alvarez/null-ls.nvim' }
+
+      use {
+          'nvim-lualine/lualine.nvim',
+          requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+      }
+    end)
