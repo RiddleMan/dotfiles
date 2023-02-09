112cfcf1a512cf973516af9d302da25bf1c7239b
Artur Ptaszek <artur.ptaszek@gmail.com>
2023-02-09 22:26:20 +0100

    feat: Add formatting fixes for null-ls and lsp-zero

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,42 +1,76 @@
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+local lsp = require("lsp-zero")
+local null_ls = require("null-ls")
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_opts = lsp.build_options("null-ls", {
+	on_attach = function(client, bufnr)
+		if client.resolved_capabilities.document_formatting then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						bufnr = bufnr,
+						filter = function(c)
+							return c.name == "null-ls"
+						end,
+					})
+				end,
+			})
+		end
+	end,
+})
+
+lsp.preset("recommended")
 
 lsp.nvim_workspace()
 lsp.ensure_installed({
-  'html',
-  'cssls',
-  'tsserver',
-  'ansiblels',
-  'bashls',
-  'dockerls',
-  'eslint',
-  'graphql',
-  'ltex',
-  'jsonls',
-  'marksman',
-  'rust_analyzer',
-  'sumneko_lua',
-  'yamlls',
-  'vimls'
+	"html",
+	"cssls",
+	"tsserver",
+	"ansiblels",
+	"bashls",
+	"dockerls",
+	"eslint",
+	"graphql",
+	"ltex",
+	"jsonls",
+	"marksman",
+	"rust_analyzer",
+	"sumneko_lua",
+	"yamlls",
+	"vimls",
 })
 
-require'lspconfig'.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = {'vim'},
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
+null_ls.setup({
+	on_attach = null_opts.on_attach,
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.rustfmt.with({
+			extra_args = { "--edition=2021" },
+		}),
+	},
+})
+
+lsp.configure("sumneko_lua", {
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
 
 lsp.setup()
