25ff998532ac468adb4c9126fad135d33952417b
Artur Ptaszek <artur.ptaszek@gmail.com>
2023-02-09 21:35:12 +0100

    fix: Inconsistent formatting in JS/TS projects

--- dot_config/nvim/after/plugin/null-ls.lua
+++ dot_config/nvim/after/plugin/null-ls.lua
@@ -1,11 +1,11 @@
-local null_ls = require "null-ls"
+local null_ls = require"null-ls"
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettier,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
@@ -14,7 +14,12 @@ null_ls.setup({
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
+          vim.lsp.buf.format({
+            bufnr = bufnr,
+            filter = function(c)
+              return c.name == "null-ls"
+            end
+          })
         end,
       })
     end
