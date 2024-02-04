4e9872ad9b94f6670e0cbe0948c34e9e3185c4d5
Artur Ptaszek <artur@ptaszek.dev>
2024-02-04 16:34:38 +0100

    feat: Refactor rustup installation

--- run_rustup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-set -e
-
-rustup update
-rustup target add aarch64-apple-ios x86_64-apple-ios aarch64-apple-ios-sim
-rustup component add clippy rustfmt
-
-ln -sf ~/.cargo/bin/cargo-binstall-raw/cargo-binstall ~/.cargo/bin/cargo-binstall
