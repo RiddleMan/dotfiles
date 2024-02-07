d1fab809fd4fd410760c4ae81f07f3d1b39e2750
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-07 16:21:17 +0100

    feat: Add tmux plugin installation for fresh systems

--- run_after_install-tpm-plugins.sh
+++ run_after_install-tpm-plugins.sh
@@ -2,4 +2,21 @@
 
 set -e
 
-~/.tmux/plugins/tpm/bin/update_plugins all
+have_plugins_been_installed() {
+  for dir in ~/.tmux/plugins/*; do
+    case $dir in
+    *tpm) continue ;;
+    *) return 0 ;;
+    esac
+  done
+
+  return 1
+}
+
+if ! have_plugins_been_installed; then
+  echo "Installing tmux plugins..."
+  ~/.tmux/plugins/tpm/bin/install_plugins
+else
+  echo "Updating tmux plugins..."
+  ~/.tmux/plugins/tpm/bin/update_plugins all
+fi
