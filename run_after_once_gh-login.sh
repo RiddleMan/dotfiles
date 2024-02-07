b73aa0b2655d98585b155a892d55a6a9c04955cc
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-07 17:47:11 +0100

    feat: Add copilot

--- run_after_once_gh-login.sh
+++ run_after_once_gh-login.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+gh extension install github/gh-copilot
+
 if ! gh auth status &>/dev/null; then
   gh auth login --git-protocol ssh --hostname github.com --web
 fi
---
+++ zsh/aliases/copilot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+set -e
+
+alias coe="gh copilot explain"
+alias cos="gh copilot suggest"
+alias coss="gh copilot suggest -t shell"
+alias cosg="gh copilot suggest -t git"
+alias cosgh="gh copilot suggest -t gh"
