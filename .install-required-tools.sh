e962a8949937d2d796c08bc5f92ae46ffd6c9fde
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-07 11:33:22 +0100

    feat: Add more required tools to initialize chezmoi

--- .install-required-tools.sh
+++ .install-required-tools.sh
@@ -14,6 +14,10 @@ has_homebrew() {
   type brew >/dev/null 2>&1
 }
 
+has_gnupg() {
+  type gpg >/dev/null 2>&1
+}
+
 # exit immediately if xcode, op and brew are already in $PATH
 has_xcode_clt && has_homebrew && has_1password && exit
 
@@ -25,10 +29,23 @@ Darwin)
 
   if ! has_homebrew; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # shellcheck disable=SC2016
+    (
+      echo
+      echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+    ) >>/Users/Artur_Ptaszek/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 
   if ! has_1password; then
     brew install --cask 1password 1password-cli
+  fi
+
+  if ! has_gnupg; then
+    brew install gnupg pinentry-mac yq
+    gpg \
+      --keyserver hkps://keys.openpgp.org \
+      --recv-keys "$(yq -r '.gpg.recipient' ~/.config/chezmoi/chezmoi.toml)"
   fi
   ;;
 *)
