8b0d266b9d0ba3a22f3dda235b1a58e489476ba6
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-07 11:41:49 +0100

    feat: Migrate to maintained version of packages

--- zsh/aliases/misc.sh
+++ zsh/aliases/misc.sh
@@ -42,7 +42,7 @@ alias gpgreset="pkill gpg-agent ; pkill ssh-agent ; pkill pinentry ; eval \$(gpg
 alias uriencode='node -e "console.log(encodeURI(process.argv[1]))"'
 alias pls="sudo !!"
 alias tmux="tmux -2"
-alias ls="exa --icons --git"
+alias ls="eza --icons --git"
 alias vim="nvim"
 
 alias tf="terraform"
