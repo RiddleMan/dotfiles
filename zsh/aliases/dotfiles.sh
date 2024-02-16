bcb4d1f0b4884ea6bf4b06a18968da5dcf0a42cf
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-16 13:00:27 +0100

    fix: DUlazy

--- zsh/aliases/dotfiles.sh
+++ zsh/aliases/dotfiles.sh
@@ -6,4 +6,4 @@ chezmoi_lazy_lock_path="$chezmoi_path/dot_config/nvim/lazy-lock.json"
 
 # we're defining an alias so it's defined in the definition not in runtime
 # shellcheck disable=SC2139
-alias dulazy="chezmoi add $lazy_lock_path && git -C $chezmoi_path add $chezmoi_lazy_lock_path && git commit -m 'feat: Update lazy-lock.json'"
+alias dulazy="chezmoi add $lazy_lock_path && git -C $chezmoi_path add $chezmoi_lazy_lock_path && git -C $chezmoi_path commit -m 'feat: Update lazy-lock.json'"
