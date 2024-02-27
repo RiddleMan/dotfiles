8758b89bcf537f04a7482d336f3890743101e514
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-27 22:27:53 +0100

    feat: Add lazy load of nvim-tree

--- dot_config/nvim/lua/plugins/tree.lua
+++ dot_config/nvim/lua/plugins/tree.lua
@@ -1,10 +1,60 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
+    lazy = true,
+    init = function()
+      require("which-key").register({
+        ["<leader>tc"] = {
+
+          function()
+            local api = require("nvim-tree.api")
+            local curr_buff = vim.api.nvim_buf_get_name(0)
+
+            api.tree.focus()
+            api.tree.find_file(curr_buff)
+          end,
+          "Locate current file in Tree View",
+        },
+        ["<c-\\>"] = {
+          function()
+            local api = require("nvim-tree.api")
+            api.tree.toggle()
+          end,
+          "Toggle Tree View",
+        },
+      })
+
+      local function open_nvim_tree(data)
+        local is_real_file = vim.fn.filereadable(data.file) == 1
+        local is_no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+        local is_gitcommit = vim.bo[data.buf].filetype == "gitcommit"
+        local is_gitrebase = vim.bo[data.buf].filetype == "gitrebase"
+        local is_directory = vim.fn.isdirectory(data.file) == 1
+
+        if
+          (not is_real_file and not is_no_name and not is_directory)
+          or is_gitcommit
+          or is_gitrebase
+        then
+          return
+        end
+
+        local api = require("nvim-tree.api")
+        if is_directory then
+          vim.cmd.cd(data.file)
+          api.tree.open()
+        else
+          api.tree.toggle({ focus = false, find_file = true })
+        end
+      end
+
+      vim.api.nvim_create_autocmd({ "VimEnter" }, {
+        callback = open_nvim_tree,
+      })
+    end,
     config = function()
-      local wk = require("which-key")
       local function my_on_attach(bufnr)
-        wk.register({
+        require("which-key").register({
           ["<leader>p"] = {
             name = "+project",
             f = {
@@ -43,49 +93,6 @@ return {
         },
 
         on_attach = my_on_attach,
-      })
-      local api = require("nvim-tree.api")
-
-      local function open_nvim_tree(data)
-        local is_real_file = vim.fn.filereadable(data.file) == 1
-        local is_no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-        local is_gitcommit = vim.bo[data.buf].filetype == "gitcommit"
-        local is_gitrebase = vim.bo[data.buf].filetype == "gitrebase"
-        local is_directory = vim.fn.isdirectory(data.file) == 1
-
-        if
-          (not is_real_file and not is_no_name and not is_directory)
-          or is_gitcommit
-          or is_gitrebase
-        then
-          return
-        end
-
-        if is_directory then
-          vim.cmd.cd(data.file)
-          api.tree.open()
-        else
-          api.tree.toggle({ focus = false, find_file = true })
-        end
-      end
-
-      vim.api.nvim_create_autocmd({ "VimEnter" }, {
-        callback = open_nvim_tree,
-      })
-
-      local function open_current_file_in_tree()
-        local curr_buff = vim.api.nvim_buf_get_name(0)
-
-        api.tree.focus()
-        api.tree.find_file(curr_buff)
-      end
-
-      wk.register({
-        ["<leader>tc"] = {
-          open_current_file_in_tree,
-          "Locate current file in Tree View",
-        },
-        ["<c-\\>"] = { api.tree.toggle, "Toggle Tree View" },
       })
     end,
     dependencies = {
