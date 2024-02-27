af311579138bf1ba6dac87b59293bf9fbf7187fb
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-27 22:01:55 +0100

    feat: Add cond on loading neodev

--- dot_config/nvim/lua/plugins/neodev.lua
+++ dot_config/nvim/lua/plugins/neodev.lua
@@ -1,16 +1,23 @@
+local is_chezmoi_path = function()
+  local path = vim.loop.fs_realpath(vim.api.nvim_buf_get_name(0))
+  if path then
+    return path:find(vim.fn.expand("~/.local/share/chezmoi"), 1, true) == 1
+  end
+  return false
+end
+
 return {
   {
     "folke/neodev.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "neovim/nvim-lspconfig",
-    },
+    cond = function()
+      local neodev_utils = require("neodev.util")
+
+      return neodev_utils.is_nvim_config() or is_chezmoi_path()
+    end,
     opts = {
-      override = function(root_dir, library)
-        if
-          root_dir:find("~/.local/share/chezmoi/dot_config/nvim", 1, true)
-          == 1
-        then
+      override = function(_, library)
+        if is_chezmoi_path() then
           library.enabled = true
           library.plugins = true
           library.types = true
