9aa69a673f4eb4b9ce2c7498dd3e368bcb41a6e9
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-27 09:58:40 +0100

    feat: Remove branch name from lua line

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -21,6 +21,16 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    },
   },
 
   { "numToStr/Comment.nvim", opts = {} },
