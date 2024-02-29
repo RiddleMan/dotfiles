acc07478fcc6d6814c5de6b330e9b785a9e1e088
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-29 13:47:37 +0100

    feat: Add close all unmodified buffers

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,20 @@
+local is_buffer_open_in_any_window = function(bufnr)
+  local is_open = false
+  local windows = vim.api.nvim_list_wins()
+  for _, winnr in ipairs(windows) do
+    if vim.api.nvim_win_get_buf(winnr) == bufnr then
+      is_open = true
+      break
+    end
+  end
+
+  return is_open
+end
+
+local is_buffer_mofiied = function(bufnr)
+  return vim.api.nvim_buf_get_option(bufnr, "modified")
+end
+
 return {
   {
     "akinsho/bufferline.nvim",
@@ -7,6 +24,23 @@ return {
       require("which-key").register({
         H = { vim.cmd.bprev, "Previous buffer" },
         L = { vim.cmd.bnext, "Next buffer" },
+        ["<leader>cab"] = {
+          function()
+            local buffers = vim.api.nvim_list_bufs()
+
+            for _, bufnr in ipairs(buffers) do
+              if
+                not is_buffer_open_in_any_window(bufnr)
+                and not is_buffer_mofiied(bufnr)
+              then
+                vim.api.nvim_buf_delete(bufnr, { force = false })
+              end
+            end
+
+            require("bufferline.ui").refresh()
+          end,
+          "Close all unmodified buffers",
+        },
       })
     end,
   },
