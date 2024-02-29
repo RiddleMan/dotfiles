cf2d67d1bd2f7bb5a9d7e9f234900c0aab5f2206
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-02-29 20:49:44 +0100

    fixup! feat: Update lazy-lock.json

--- zsh/aliases/dotfiles.sh
+++ zsh/aliases/dotfiles.sh
@@ -4,7 +4,7 @@ chezmoi_path="$HOME/.local/share/chezmoi"
 lazy_lock_path="$HOME/.config/nvim/lazy-lock.json"
 chezmoi_lazy_lock_path="$chezmoi_path/dot_config/nvim/lazy-lock.json"
 
-alias dulazy='chezmoi add "$lazy_lock_path" && git -C $chezmoi_path add "$chezmoi_lazy_lock_path" && git -C "$chezmoi_path commit" -m '\''feat: Update lazy-lock.json'\'''
+alias dulazy='chezmoi add "$lazy_lock_path" && git -C $chezmoi_path add "$chezmoi_lazy_lock_path" && git -C "$chezmoi_path" commit -m '\''feat: Update lazy-lock.json'\'''
 
 dash_path="$chezmoi_path/.dash"
 alias dudash='git -C $chezmoi_path add $dash_path && git -C $chezmoi_path commit -m '\''feat: Update Dash.dashsync'\'''
