86e24d71b66f28ad915f5542f7936ab6704f8f4c
Artur Ptaszek <artur.ptaszek@gmail.com>
2025-02-06 15:14:53 +0100

    feat: Update Neovim

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
       },
       {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        build = "make",
       },
     },
 
